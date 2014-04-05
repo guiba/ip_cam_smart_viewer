d619e2e5ec26482ace0d268f156d2907bcf43502
guiba <mas.floris@gmail.com>
2014-04-05 13:42:11 +0200

    add methods for building list and get test from tags

--- xml_parser.py
+++ xml_parser.py
@@ -1,14 +1,38 @@
+#!/usr/bin/env python
+
 from xml.dom import minidom
+import urllib
 
-class xmlParser( url):
-        def __init__(url=None):
-                url = url
+class XmlParser:
+        def __init__(self, source):                
                 print "parsing stream list..."
-                xmldoc = minidom.parse('~/home/pi/ipcam/stream_list.xml ')
+		self.url = source
+		usock = urllib.urlopen(source)
+                self.xmldoc = minidom.parse(usock)
+		usock.close()
+		print self.xmldoc.toxml()
                 print "parsed ok"
-        def get_url_list():
-                listNode = xmldoc.firstChild
-                index = 0
-                print listNode.toxml()
-                for cam in listNode.childNodes:
-
+        def build_url_list(self):
+		self.list = []
+                print "building list"
+		cams =self.xmldoc.getElementsByTagName('rem_addr')
+                for cam in cams:
+			addr = cam.attributes['href']
+			print "Adding Cam: %s" % addr.value
+			self.list.append(addr.value)
+		return self.list
+	def getText(self, nodelist):
+    		rc = []
+    		for node in nodelist:
+        		if node.nodeType == node.TEXT_NODE:
+            			rc.append(node.data)
+    		return ''.join(rc)	
+def main():
+	cameras = 'stream_list.xml'
+	p = XmlParser(cameras)
+	list = p.build_url_list()
+	print "here is the ful list"
+	for item in list:
+		print item
+if __name__ == '__main__':
+    main()
