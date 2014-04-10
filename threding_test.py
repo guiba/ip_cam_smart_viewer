36b2e1d18c1640e2f2079d00b4fc685d607c2e14
guiba <mas.floris@gmail.com>
2014-04-10 22:20:35 +0000

    reading remot codes using libcec and threading

---
+++ threding_test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from subprocess import Popen, PIPE
+from time import sleep
+from nbstreamreader import NonBlockingStreamReader as NBSR
+
+# run the shell as a subprocess:
+p = Popen('cec-client',
+        stdin = PIPE, stdout = PIPE, stderr = PIPE, shell = False)
+# wrap p.stdout with a NonBlockingStreamReader object:
+nbsr = NBSR(p.stdout)
+# issue command:
+#p.stdin.write('command\n')
+# get the output
+while True:
+    output = nbsr.readline()
+    # 0.1 secs to let the shell output the result
+    if output:
+      if(output.startswith('waiting')):
+	print "Ready for input"
+      if(output.startswith('TRAFFIC:')):
+	code = output.split('>>')
+	if (len(code) > 1):
+	  print code[1]
+      
+      
+def check_line(line):
+  if(line.startswith('TRAFIC:')):
+       code = line.split('>>')[1]
+       return code;
+       
+    
+    
